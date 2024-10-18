d2a9bb511362a329fb9c204474ba89c7d2e5e57d
thisisrealnoman <nomanontheroad@gmail.com>
2024-10-18 21:18:43 +0600

    extract common code into functions

--- scripts/parallelogram.js
+++ scripts/parallelogram.js
@@ -1,14 +1,33 @@
+// function calculateParallelogramArea(){
+//     const parallelogramBaseInputField = document.getElementById('parallelogram-base');
+//     const parallelogramBaseText = parallelogramBaseInputField.value;
+//     const base = parseFloat(parallelogramBaseText);
+
+//     const parallelogramHeightInputField = document.getElementById('parallelogram-height');
+//     const parallelogramHeightText = parallelogramHeightInputField.value;
+//     const height = parseFloat(parallelogramHeightText);
+
+//     const area = base * height;
+
+//     const parallelogramAreaSpan = document.getElementById('parallelogram-area');
+//     parallelogramAreaSpan.innerText = area;
+// }
+
 function calculateParallelogramArea(){
-    const parallelogramBaseInputField = document.getElementById('parallelogram-base');
-    const parallelogramBaseText = parallelogramBaseInputField.value;
-    const base = parseFloat(parallelogramBaseText);
-
-    const parallelogramHeightInputField = document.getElementById('parallelogram-height');
-    const parallelogramHeightText = parallelogramHeightInputField.value;
-    const height = parseFloat(parallelogramHeightText);
-
+    const base = getInputValueById('parallelogram-base');
+    const height = getInputValueById('parallelogram-height');
     const area = base * height;
+    setInnerTextById('parallelogram-area', area);
+}
 
-    const parallelogramAreaSpan = document.getElementById('parallelogram-area');
-    parallelogramAreaSpan.innerText = area;
+function getInputValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputValueText = inputField.value;
+    const inputValue = parseFloat(inputValueText);
+    return inputValue;
+}
+
+function setInnerTextById(elementId, areaValue){
+    const element = document.getElementById(elementId);
+    element.innerText = areaValue;
 }
